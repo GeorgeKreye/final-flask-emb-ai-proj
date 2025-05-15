109f62ac0e329aba3947e33f556828fb4e07be49
GeorgeKreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-05-15 19:17:49 -0400

    Additional linting & documentation for portfolio use

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,22 +1,50 @@
+"""
+Module that is used to IBM Watson's AI emotion detector API and 
+return extracted results.
+"""
+
+# imports
 import json
 import requests
 
 def get_dominant_emotion(emotion_data):
+    """
+    Helper function that finds the dominant (highest value)
+    emotion from given emotion data.
+    """
+    # create holder for highest-rated emotion
     highest = ('', -1)
+
+    # iterate through emotion data
     for emotion, value in zip(emotion_data.keys(), emotion_data.values()):
+        # update highest-rated emotion if a higher value is found
         if value > highest[1]:
             highest = (emotion, value)
+
+    # return name of highest-rated emotion
     return highest[0]
 
 
 def emotion_detector(text_to_analyze):
+    """
+    Given a text string to analyze, calls the Watson AI API
+    to perform emotion detection. Returns a dictionary containing
+    each emotion's rating and the dominant emotion.
+
+    If the input is invalid, returns the samed dictionary wtih
+    all values set to null.
+    """
+    # setup parameters for web request
     url = ('https://sn-watson-emotion.labs.skills.network/' +
      'v1/watson.runtime.nlp.v1/NlpService/EmotionPredict')
     inp = {"raw_document": {"text": text_to_analyze}}
     headers = {"grpc-metadata-mm-model-id":
      "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json=inp, headers=headers)
+
+    # make web request
+    response = requests.post(url, json=inp, headers=headers, timeout=60)
     if response.status_code == 400:
+        # bad input, return null values
         return {
             'anger': None,
             'disgust': None,
@@ -25,6 +53,8 @@ def emotion_detector(text_to_analyze):
             'sadness': None,
             'dominant_emotion': None
         }
+
+    # extract & format data for returning
     data = json.loads(response.text)['emotionPredictions'][0]['emotion']
     return {
         'anger': data['anger'],
--- server.py
+++ server.py
@@ -2,21 +2,29 @@
 Server for final project web application.
 """
 
+# imports
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
+# instantiate Flask app
 app = Flask("Emotion Detector")
 
+# Create route for the web app to call the emotion detector function
 @app.route("/emotionDetector")
 def detect_emotion():
     """
     Calls Watson AI emotion detector function from the
     EmotionDetector package
     """
+    # get text to analyze
     text_to_analyze = request.args.get('textToAnalyze')
+
+    # call emotion detector function, catching bad input
     result = emotion_detector(text_to_analyze)
     if result['dominant_emotion'] is None:
         return "Invalid text! Please try again."
+
+    # return formatted string containing detector result data
     return (
         "For the given statement, the system response is " +
         f"'anger': {result['anger']}, 'digust': {result['disgust']}, " +
@@ -25,12 +33,15 @@ def detect_emotion():
         f"{result['dominant_emotion']}."
     )
 
+# Create base route for main apge
 @app.route("/")
 def render_index_page():
     """
     Renders landing page of web application.
     """
+    # Render landing page stored at index.html
     return render_template("index.html")
 
+# run application at localhost:5000 if this file is called
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
